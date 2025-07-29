3c2bb3c4342407d789ef6209ae8120167e09af9e
Giuseppe Roberto Lombardo <ukg15425@kireygroup.com>
2025-07-29 10:20:28 +0200

    add threadpoolexecutor with submit example

---
+++ multithreading/coreyschafer/02-threadpool-submit.py
@@ -0,0 +1,30 @@
+from threading import Thread, Lock, current_thread
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+
+SECONDS = 1
+THREADS = 10
+
+
+def worker(seconds:int) -> str:
+    name = current_thread().name
+    id = current_thread().ident
+    print(f'worker: {name}:{id}, sleeping {seconds=}')
+    time.sleep(seconds)
+    return f'worker: {name}:{id}, done'
+
+
+if __name__ == '__main__':
+    print('main thread: starting...')
+    start = time.perf_counter()
+
+    with ThreadPoolExecutor() as executor:
+        results = [executor.submit(worker, SECONDS) for _ in range(THREADS)]
+        for f in results:
+            print(f.result())
+
+    stop = time.perf_counter()
+    elapsed = stop - start
+    print(f'main thread: finished in {elapsed=:.2f} seconds')
+
